8fea1ae4c9149fd1719c50b47215ed4d2076b02f
Logulasse <maxence.tasse@gmail.com>
2023-07-29 19:40:23 +0200

    Add firstName and lastName in User.php entity. Add Country.php entity. Add migration.

---
+++ src/Entity/Country.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CountryRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CountryRepository::class)]
+class Country
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 10, nullable: true)]
+    private ?string $phoneCode = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPhoneCode(): ?string
+    {
+        return $this->phoneCode;
+    }
+
+    public function setPhoneCode(?string $phoneCode): static
+    {
+        $this->phoneCode = $phoneCode;
+
+        return $this;
+    }
+}
