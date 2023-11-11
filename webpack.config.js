0fc2dbd040c8e8ca2bd3544416224decc5485ec6
maxencetasse <maxence.tasse@gmail.com>
2023-11-11 19:19:11 +0100

    Add button style on roles page Add bootstrap icons for all the project

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,19 @@ if (!Encore.isRuntimeEnvironmentConfigured()) {
     Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
 }
 
+module.exports = {
+    module: {
+        rules: [{
+            test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/i,
+            type: 'asset/resource',
+            generator: {
+                //filename: 'fonts/[name]-[hash][ext][query]'
+                filename: 'fonts/[name][ext][query]'
+            }
+        }]
+    }
+}
+
 Encore
     // directory where compiled assets will be stored
     .setOutputPath('public/build/')
