f678b7433a6c6325713c7c49dfb95669467e7ad2
maxencetasse <maxence.tasse@gmail.com>
2024-06-05 20:51:31 +0200

    Adding Node entity and it's migration Adding constructor to XMLManagerService Adding link between Document.php and Node.php

---
+++ src/Entity/Node.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NodeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: NodeRepository::class)]
+class Node
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $type = null;
+
+    #[ORM\ManyToOne(inversedBy: 'nodes')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Document $document = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(?string $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getDocument(): ?Document
+    {
+        return $this->document;
+    }
+
+    public function setDocument(?Document $document): static
+    {
+        $this->document = $document;
+
+        return $this;
+    }
+}
--- src/Service/XMLManagerService.php
+++ src/Service/XMLManagerService.php
@@ -2,7 +2,13 @@
 
 namespace App\Service;
 
+use App\Repository\DocumentRepository;
+
 class XMLManagerService
 {
-
+    private DocumentRepository $documentRepository;
+    public function __construct(DocumentRepository $documentRepository)
+    {
+        $this->documentRepository = $documentRepository;
+    }
 }
