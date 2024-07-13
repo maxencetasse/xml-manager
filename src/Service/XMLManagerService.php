133b47c09ef96d25f338898be351012804f83fd5
maxencetasse <maxence.tasse@gmail.com>
2024-07-13 19:40:32 +0200

    Adding firstReadXMLDocument() and readXMLDocument() method to XMLManagerService.php

--- src/Service/XMLManagerService.php
+++ src/Service/XMLManagerService.php
@@ -2,13 +2,55 @@
 
 namespace App\Service;
 
+use App\Entity\Document;
 use App\Repository\DocumentRepository;
+use App\Repository\NodeRepository;
 
 class XMLManagerService
 {
     private DocumentRepository $documentRepository;
-    public function __construct(DocumentRepository $documentRepository)
+    private NodeRepository $nodeRepository;
+    private Document $document;
+    public function __construct(DocumentRepository $documentRepository, NodeRepository $nodeRepository)
     {
         $this->documentRepository = $documentRepository;
+        $this->nodeRepository = $nodeRepository;
+    }
+
+    public function initialize(Document $document): void
+    {
+        $this->document = $document;
+    }
+
+    public function firstReadXMLDocument(): array
+    {
+        $nodes = [];
+
+        \XMLReader::open($this->document->getName());
+        while (\XMLReader::read()) {
+            if (\XMLReader::$nodeType === \XMLReader::ELEMENT) {
+                $nodes[] = [
+                    'name' => \XMLReader::$name,
+                    'type' => \XMLReader::$nodeType,
+                    'value' => \XMLReader::$value,
+                ];
+            }
+        }
+
+        return $nodes;
+    }
+
+    public function readXMLDocument(): void
+    {
+        \XMLReader::open($this->document->getName());
+        while (\XMLReader::read()) {
+            if (\XMLReader::$nodeType === \XMLReader::ELEMENT) {
+                $node = new Node();
+                $node->setName(\XMLReader::$name);
+                $node->setType(\XMLReader::$nodeType);
+                $node->setDocument($this->document);
+                $this->nodeRepository->save($node);
+            }
+        }
     }
 }
